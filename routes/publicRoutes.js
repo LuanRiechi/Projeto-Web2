24f3b972e42daeaf088c09547709f6f7e2c442da
LuanRiechi <luanriechi@alunos.utfpr.edu.br>
2023-06-19 18:43:57 -0300

    Criando as demais rotas publicas do ciclo 1, criando a estrtura do templete de registrar e dando estilizando o mesmo

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/home',(req,res)=>{
+router.get('/',(req,res)=>{
     res.render('public/home')
 })
+
+router.get('/sobre',(req,res)=>{
+    res.render('public/sobre')
+})
+
+router.get('/tecnologias',(req,res)=>{
+    res.render('public/tecnologias')
+})
+
+router.get('/desenvolvedor',(req,res)=>{
+    res.render('public/desenvolvedor')
+})
+
+router.get('/contato',(req,res)=>{
+    res.render('public/contato')
+})
+
+router.get('/registrar',(req,res)=>{
+    res.render('public/registrar')
+})
+
+
+
 
 module.exports = router
