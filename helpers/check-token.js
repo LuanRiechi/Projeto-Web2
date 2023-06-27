d48532c34d859dce160af6551ab3621b660fa9ea
LuanRiechi <luanriechi@alunos.utfpr.edu.br>
2023-06-27 13:10:27 -0300

    ajustando a logica de autenticação

--- helpers/check-token.js
+++ helpers/check-token.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-const checkToken = (req, res, next) => {
+const  checkToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
@@ -8,8 +9,15 @@ const checkToken = (req, res, next) => {
 
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
-    next();
+    const userId = verified.id;
+    const user = await User.findOne({ _id: userId });
+    if(user){
+        req.user = verified;
+        next();
+    } else{
+        res.status(400).json({ status: false, mensagem: "Não existe usuario com esse token" });
+        return
+    }
   } catch (err) {
     res.status(400).json({ status: false, mensagem: "O Token é inválido!" });
   }
