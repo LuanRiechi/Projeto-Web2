65151714560d2956cfe1fdd725be443fc0de683d
LuanRiechi <luanriechi@alunos.utfpr.edu.br>
2023-06-27 12:07:32 -0300

    arrumando as mensagens de erro

--- helpers/check-token.js
+++ helpers/check-token.js
@@ -4,14 +4,14 @@ const checkToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.status(401).json({ message: "Acesso negado!" });
+  if (!token) return res.status(401).json({ status: false, mensagem: "Acesso negado!" });
 
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     next();
   } catch (err) {
-    res.status(400).json({ message: "O Token é inválido!" });
+    res.status(400).json({ status: false, mensagem: "O Token é inválido!" });
   }
 };
 
--- helpers/get-user-by-token.js
+++ helpers/get-user-by-token.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const getUserByToken = async (token) => {
-  if (!token) return res.status(401).json({ error: "Acesso negado!" });
+  if (!token) return res.status(401).json({ status: false, mensagem: "Acesso negado!" });
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
